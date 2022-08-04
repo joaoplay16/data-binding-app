0f911bf3f53f92e499019234584f3113dbbf5137
João Pedro <joaoplay16@gmail.com>
2022-08-03 21:52:00 -0300

    Includes - Data Binding

---
+++ app/src/main/java/com/playlab/databindingapp/IncludesActivity.kt
@@ -0,0 +1,11 @@
+package com.playlab.databindingapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class IncludesActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_includes)
+    }
+}
